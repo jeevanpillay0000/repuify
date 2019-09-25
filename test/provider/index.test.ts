51889a3f26166c2421eff985255540f6a9afdb0f
lpfloyd <jeevan.pillay8@gmail.com>
2019-09-25 15:20:41 +1000

    Formmated provider tests for better visiblity

--- test/provider/index.test.ts
+++ test/provider/index.test.ts
@@ -1,65 +1,76 @@
-'use strict'
-import { expect } from 'chai'
-import { RepuProvider } from '../../src/provider'
+"use strict";
+import { expect } from "chai";
+import { RepuProvider } from "../../src/provider";
 
-describe('thor-provider initialization', () => {
+describe("thor-provider initialization", () => {
+  it("should throw error if called without empty host", () => {
+    expect(() => {
+      const provider = new RepuProvider("");
+      return provider;
+    }).to.throw(
+      '[thor-provider]Thorify requires that the host be specified(e.g. "http://localhost:8669")'
+    );
+  });
 
-    it('should throw error if called without empty host', () => {
-        expect(() => { const provider = new RepuProvider(''); return provider }).to.throw('[thor-provider]Thorify requires that the host be specified(e.g. "http://localhost:8669")')
-    })
+  it("should parse url", () => {
+    const provider = new RepuProvider("http://localhost:8669");
+    expect(provider).to.have.property("RESTHost", "http://localhost:8669");
+  });
 
-    it('should parse url', () => {
-        const provider = new RepuProvider('http://localhost:8669')
-        expect(provider).to.have.property('RESTHost', 'http://localhost:8669')
-    })
+  it("should parse url with https", () => {
+    const provider = new RepuProvider("https://localhost:8669");
+    expect(provider).to.have.property("RESTHost", "https://localhost:8669");
+  });
 
-    it('should parse url with https', () => {
-        const provider = new RepuProvider('https://localhost:8669')
-        expect(provider).to.have.property('RESTHost', 'https://localhost:8669')
-    })
+  it("should throw error with wrong", () => {
+    expect(() => {
+      return new RepuProvider("invalidurl");
+    }).to.throw("[thor-provider]Parsing url failed!");
+  });
+});
 
-    it('should throw error with wrong', () => {
-        expect(() => {
-            return new RepuProvider('invalidurl')
-        }).to.throw('[thor-provider]Parsing url failed!')
-    })
+describe("thor-provider methods", () => {
+  it("not supported method should return error ", done => {
+    const provider = new RepuProvider("http://localhost:8669");
+    provider.sendAsync(
+      {
+        method: "not supported method"
+      },
+      (err, ret) => {
+        if (err) {
+          done(new Error("Return error in wrong place!"));
+        }
+        if (ret.error) {
+          expect(ret.error.message).to.be.equal(
+            "[thor-provider]Method not supported!"
+          );
+          done();
+        } else {
+          done(new Error("No error thrown!"));
+        }
+      }
+    );
+  });
 
-})
-
-describe('thor-provider methods', () => {
-
-    it('not supported method should return error ', (done) => {
-        const provider = new RepuProvider('http://localhost:8669')
-        provider.sendAsync({
-            method: 'not supported method',
-        }, (err, ret) => {
-            if (err) {
-                done(new Error('Return error in wrong place!'))
-            }
-            if (ret.error) {
-                expect(ret.error.message).to.be.equal('[thor-provider]Method not supported!')
-                done()
-            } else {
-                done(new Error('No error thrown!'))
-            }
-        })
-    })
-
-    it('eth_sendTransaction method should throw error', (done) => {
-        const provider = new RepuProvider('http://localhost:8669')
-        provider.sendAsync({
-            method: 'eth_sendTransaction',
-        }, (err, ret) => {
-            if (err) {
-                done(new Error('Return error in wrong place!'))
-            }
-            if (ret.error) {
-                expect(ret.error.message).to.be.equal('[thor-provider]The private key corresponding to from filed can\'t be found in local eth.accounts.wallet!')
-                done()
-            } else {
-                done(new Error('No error thrown!'))
-            }
-        })
-    })
-
+  it("eth_sendTransaction method should throw error", done => {
+    const provider = new RepuProvider("http://localhost:8669");
+    provider.sendAsync(
+      {
+        method: "eth_sendTransaction"
+      },
+      (err, ret) => {
+        if (err) {
+          done(new Error("Return error in wrong place!"));
+        }
+        if (ret.error) {
+          expect(ret.error.message).to.be.equal(
+            "[thor-provider]The private key corresponding to from filed can't be found in local eth.accounts.wallet!"
+          );
+          done();
+        } else {
+          done(new Error("No error thrown!"));
+        }
+      }
+    );
+  });
 });
--- test/provider/simple-http.test.ts
+++ test/provider/simple-http.test.ts
@@ -1,75 +1,82 @@
-'use strict'
-import { xhrUtility } from '../test-utils/init'
+"use strict";
+import { xhrUtility } from "../test-utils/init";
+import { expect } from "chai";
+import { HTTP } from "../../src/provider/simple-http";
 
-import {expect} from 'chai'
-import {HTTP} from '../../src/provider/simple-http'
+describe("simple http", () => {
+  it("get", async () => {
+    HTTP.get("http://localhost:8669/test");
 
-describe('simple http', () => {
-    it('get', async () => {
-        HTTP.get('http://localhost:8669/test')
+    const { url } = xhrUtility.extractRequest();
+    expect(url).to.be.equal("/test");
+  });
 
-        const { url } = xhrUtility.extractRequest()
-        expect(url).to.be.equal('/test')
-    })
+  it("post", async () => {
+    HTTP.post("http://localhost:8669/test", { data: "body" });
 
-    it('post', async () => {
-        HTTP.post('http://localhost:8669/test', {data: 'body'})
+    const { url, body } = xhrUtility.extractRequest();
 
-        const { url, body } = xhrUtility.extractRequest()
+    expect(url).to.be.equal("/test");
+    expect(body).to.have.property("data", "body");
+  });
+});
 
-        expect(url).to.be.equal('/test')
-        expect(body).to.have.property('data', 'body')
-    })
-})
+describe("simple http: error handling", () => {
+  it("parse error should throw error", done => {
+    xhrUtility.setError("ParseError");
+    HTTP.get("http://localhost:8669/test")
+      .then(() => {
+        done(new Error("no error thrown"));
+      })
+      .catch(e => {
+        try {
+          expect(() => {
+            throw e || "no error";
+          }).to.throw(
+            "[thor-provider]Error parsing the response: Unexpected token t in JSON at position 1"
+          );
+          done();
+        } catch (err) {
+          done(err);
+        }
+      });
+  });
 
-describe('simple http: error handling', () => {
+  it("connect error should throw error", done => {
+    xhrUtility.setError("ConnectError");
+    HTTP.get("http://localhost:8669/test")
+      .then(() => {
+        done(new Error("no error thrown"));
+      })
+      .catch(e => {
+        try {
+          expect(() => {
+            throw e || "no error";
+          }).to.throw("[thor-provider]Connect error: send failed");
+          done();
+        } catch (err) {
+          done(err);
+        }
+      });
+  });
 
-    it('parse error should throw error', (done) => {
-
-        xhrUtility.setError('ParseError')
-        HTTP.get('http://localhost:8669/test').then(() => {
-            done(new Error('no error thrown'))
-        }).catch((e) => {
-            try {
-                expect(() => { throw e || 'no error' }).to.throw('[thor-provider]Error parsing the response: Unexpected token t in JSON at position 1')
-                done()
-            } catch (err) {
-                done(err)
-            }
-        })
-
-    })
-
-    it('connect error should throw error', (done) => {
-
-        xhrUtility.setError('ConnectError')
-        HTTP.get('http://localhost:8669/test').then(() => {
-            done(new Error('no error thrown'))
-        }).catch((e) => {
-            try {
-                expect(() => { throw e || 'no error' }).to.throw('[thor-provider]Connect error: send failed')
-                done()
-            } catch (err) {
-                done(err)
-            }
-        })
-
-    })
-
-    it('timeout error should throw error', (done) => {
-
-        xhrUtility.setError('TimeoutError')
-        HTTP.get('http://localhost:8669/test').then(() => {
-            done(new Error('no error thrown'))
-        }).catch((e) => {
-            try {
-                expect(() => { throw e || 'no error' }).to.throw('[thor-provider]Time out for whatever reason, check your provider')
-                done()
-            } catch (err) {
-                done(err)
-            }
-        })
-
-    })
-
-})
+  it("timeout error should throw error", done => {
+    xhrUtility.setError("TimeoutError");
+    HTTP.get("http://localhost:8669/test")
+      .then(() => {
+        done(new Error("no error thrown"));
+      })
+      .catch(e => {
+        try {
+          expect(() => {
+            throw e || "no error";
+          }).to.throw(
+            "[thor-provider]Time out for whatever reason, check your provider"
+          );
+          done();
+        } catch (err) {
+          done(err);
+        }
+      });
+  });
+});
